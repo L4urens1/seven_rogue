78a8b9dbe7ed059ac29ebfa52bfe4b85a840bfc3
L4urens1 <bhen5808@gmail.com>
2025-08-12 15:59:41 +0300

    Add files via upload

---
+++ src/main/java/me/_l4urens_/seven/Seven.java
@@ -0,0 +1,32 @@
+package me._l4urens_.seven;
+
+import me._l4urens_.seven.commends.boom;
+import me._l4urens_.seven.commends.next;
+import me._l4urens_.seven.commends.startboom;
+import me._l4urens_.seven.commends.worldEditApiTest;
+import me._l4urens_.seven.listners.onDeath;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public final class Seven extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        saveResource("house.schem", false);
+    }
+    @Override
+    public void onEnable() {
+
+        // Plugin startup logic
+//        getServer().getPluginManager().registerEvents(new onDeath(), this);
+        System.out.println(ChatColor.GREEN+"The plugin had enabled");
+        getCommand("boomStart").setExecutor(new startboom());
+        getCommand("next").setExecutor(new next());
+        getCommand("boom").setExecutor(new boom());
+        getCommand("apiTest").setExecutor(new worldEditApiTest(this));
+
+
+    }
+
+}
---
+++ src/main/java/me/_l4urens_/seven/commends/next.java
@@ -0,0 +1,65 @@
+package me._l4urens_.seven.commends;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class next implements CommandExecutor {
+    private final Map<UUID, Integer> allowedNextPage = new HashMap<>();
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if(sender instanceof Player p){
+        Integer num = Integer.parseInt(p.getScoreboard().getEntries().toArray()[0].toString());
+           if(!p.getScoreboardTags().contains("can use")){p.sendMessage(ChatColor.RED+"u cant use this command now"); return false;}
+            if (num % 7 == 0 || Integer.toString(num).contains("7")) {
+                p.sendMessage(ChatColor.BOLD+(ChatColor.RED+"GAME OVER"));
+                p.removeScoreboardTag("can use");
+
+            }else {
+                p.sendMessage(ChatColor.BOLD+(ChatColor.GREEN+"CORRECT"));
+                num += 1;
+
+                TextComponent option1 = new TextComponent("next");
+                option1.setColor(net.md_5.bungee.api.ChatColor.GREEN);
+                option1.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND ,"/next"));
+                option1.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to get next number").create()));
+
+                TextComponent option2 = new TextComponent("boom");
+                option2.setColor(net.md_5.bungee.api.ChatColor.RED);
+                option2.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "boom"));
+                option2.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to boom the number").create()));
+
+                TextComponent message = new TextComponent(" ");
+                message.addExtra(option1);
+                message.addExtra(" or ");
+                message.addExtra(option2);
+                p.sendMessage(Integer.toString(num));
+                p.spigot().sendMessage(message);
+                ScoreboardManager manager = Bukkit.getScoreboardManager();
+                Scoreboard scoreboard = manager.getNewScoreboard();
+
+                Objective objective = scoreboard.registerNewObjective("num", "dummy");
+                Score score = objective.getScore(num.toString());
+                objective.setDisplayName("num");
+                score.setScore(1);
+                p.setScoreboard(scoreboard);
+            }
+        }
+        return true;
+    }
+}
